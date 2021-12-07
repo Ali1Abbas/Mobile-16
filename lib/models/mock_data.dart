dd1eb995c9eb49ee55905e3cd0982d0f2efbe017
Ali1Abbas <abbas.a487567@gmail.com>
2021-12-07 13:26:10 +0300

    i have create mock data

--- lib/models/mock_data.dart
+++ lib/models/mock_data.dart
@@ -2,10 +2,19 @@
 
 import 'note.dart';
 
-final noteList = <Note>[];
+final noteList = <Note>[
+  Note("What is Flutter ",
+      "Flutter is an open-source UI framework for creating native mobile applications. It allows developers to build mobile applications a single codebase."),
+  Note("Stateful Widgets",
+      "The widgets whose state can be altered once they are built are called stateful Widgets. "),
+  Note("Conditional (ternary) operator ",
+      "The conditional (ternary) operator in Dart takes three operands: a condition, first expression (if truthy) and second expression (if falsy).")
+];
 
 //? Here are some sample data that you can copy-paste in order to create the mock database
 //? --------------------------------------------------------------------------------------
+
+
 
 /*
 What is Flutter?
