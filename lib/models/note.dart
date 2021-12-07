31b72e22fb182eeb78b15d0b57e95c0b0d10f6d4
Ali1Abbas <abbas.a487567@gmail.com>
2021-12-07 13:21:45 +0300

    ihave create class note and copy it

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,3 +1,10 @@
 //! Define the model class Note here
 
-class Note {}
+class Note {
+  String title;
+  String content;
+
+  Note(this.title, this.content);
+
+  Note.copy(Note obj) : this((obj.title), (obj.content));
+}
